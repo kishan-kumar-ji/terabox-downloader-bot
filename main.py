ee7e4e3ad6e828fb7fd61239bf280bbe9b7c3fbf
kishan-kumar-ji <158287711+kishan-kumar-ji@users.noreply.github.com>
2024-02-11 23:30:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from tools import (
 bot = Client("pyro", api_id, api_hash, bot_token=bot_token)
 
 
-@bot.on_message(filters=filters.command("start") & filters.private)
+@bot.on_message(filters=filters.command("start"))
 async def start(m: Message):
     await m.reply(
         "Hello! I am a bot to download videos from terabox.\nSend me the terabox link and i will start downloading it. \nJoin @RoldexVerse For Updates. :)",
@@ -25,39 +25,25 @@ async def start(m: Message):
     )
 
 
-@bot.on_message(filters.text & filters.private)
+@bot.on_message(filters.text)
 async def echo_message(_, m: Message):
-    check_if = await is_user_on_chat(bot, -1001345686737, m.from_user.id)
-    if not check_if:
-        return await m.reply_text(
-            "Please join @RoldexVerse then send me the link again.",
-            reply_to_message_id=m.id,
-        )
-    check_if = await is_user_on_chat(bot, -1001320804136, m.from_user.id)
-    if not check_if:
-        return await m.reply_text(
-            "Please join @RoldexVerseChats then send me the link again.",
-            reply_to_message_id=m.id,
-        )
+
 
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply_text("Please enter a valid url.", reply_to_message_id=m.id)
-    hm = await m.reply_text("Sending you the media wait...", reply_to_message_id=m.id)
+    hm = await m.reply_text("Sending you the video wait...", reply_to_message_id=m.id)
     data = get_data(url)
 
     if data is False:
         return await hm.edit("Sorry! API is dead or maybe your link is broken.")
-    if data["sizebytes"] > 524288000 and m.chat.id not in [1317173146]:
-        return await hm.edit(
-            f"Sorry! File is too big. I can download only 500MB and this file is of {data['size']} ."
-        )
+    
 
     is_downloading = get_current_downloading(data["file_name"])
     if not is_downloading:
         init_download(data["link"])
     hm = await hm.edit(
-        f"Downloading {data['size']}...\nYou can download through the <a href={data['link']}>{data['file_name']}</a>.\nI am also sending you a telegram video of this video.",
+        f"Downloading {data['size']}..{data['file_name']}</a>.\nI am  sending you a telegram video of this video.\n made by kxzen x",
         parse_mode=ParseMode.HTML,
         disable_web_page_preview=True,
     )
